0f61f86cf54bc3a29f97d56f3fcff8f84644f23c
henrriusdev <henrrybrgt@gmail.com>
2024-10-20 16:50:45 -0400

    feat: Add fields to User and Role structs
    Add new fields to the User and Role structs in the model package. The User struct now includes fields for username, password, email, first name, last name, date of birth, address, and phone number. The Role struct now includes fields for name and description.

--- pkg/model/entity.go
+++ pkg/model/entity.go
@@ -9,10 +9,20 @@ type BaseEntity struct {
 
 type User struct {
 	BaseEntity
+	Username  string `json:"username" db:"username"`
+	Password  string `json:"password" db:"password"`
+	Email     string `json:"email" db:"email"`
+	FirstName string `json:"first_name" db:"first_name"`
+	LastName  string `json:"last_name" db:"last_name"`
+	DOB       string `json:"dob" db:"dob"`
+	Address   string `json:"address" db:"address"`
+	Phone     string `json:"phone" db:"phone"`
 }
 
 type Role struct {
 	BaseEntity
+	Name        string `json:"name" db:"name"`
+	Description string `json:"description" db:"description"`
 }
 
 type Application struct {
