3c0bc8df0213661fb474edd50e85de36f17092d3
henrriusdev <henrrybrgt@gmail.com>
2024-10-20 16:19:59 -0400

    feat: Add config package for environment variables
    - Create config package with EnvVar struct to hold environment variables
    - Add LoadEnv function to load environment variables from .env file
    - Use caarlos0/env/v6 and joho/godotenv packages for environment variable parsing
    
    Related to #issue_number

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"log"
+	"path/filepath"
+	"runtime"
+
+	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
+)
+
+type EnvVar struct {
+	Env       string `env:"ENV" envDefault:"local"`
+	BaseURL   string `env:"BASE_URL" envDefault:"http://localhost"`
+	Port      string `env:"PORT" envDefault:"3000"`
+	DBName    string `env:"DB_NAME" envDefault:"neploy"`
+	DBUser    string `env:"DB_USER" envDefault:"postgres"`
+	DBPass    string `env:"DB_PASS" envDefault:"Reyshell"`
+	DBHost    string `env:"DB_HOST" envDefault:"localhost"`
+	DBPort    string `env:"DB_PORT" envDefault:"5432"`
+	JWTSecret string `env:"JWT_SECRET" envDefault:"secret"`
+}
+
+var Env EnvVar
+
+func getEnvPath() string {
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+	return filepath.Join(basepath, "..", ".env")
+}
+
+func LoadEnv() {
+	if err := godotenv.Load(getEnvPath()); err != nil {
+		log.Println("No .env file found")
+	}
+
+	if err := env.Parse(&Env); err != nil {
+		log.Fatalf("Failed to parse env: %v", err)
+	}
+}
