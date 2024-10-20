fa6f01df3db6a38af40bb39ef270ea2332c27d2e
henrriusdev <henrrybrgt@gmail.com>
2024-10-20 16:47:04 -0400

    refactor: Refactor main.go to use config package and sqlx for database operations

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"neploy.dev/config"
 	"neploy.dev/neploy"
+	"neploy.dev/pkg/store"
 )
 
 func main() {
-	neploy.Start()
+	config.LoadEnv()
+
+	db, _ := store.NewConnection(config.Env)
+	npy := neploy.Neploy{
+		DB:   db,
+		Port: config.Env.Port,
+	}
+
+	neploy.Start(npy)
 }
