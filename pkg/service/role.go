6e577e8b2ae4a2f92956993c8c69bf869de49fcb
henrriusdev <henrrybrgt@gmail.com>
2024-10-25 07:04:53 -0400

    refactor: Add Role CRUD operations to role service

--- pkg/service/role.go
+++ pkg/service/role.go
@@ -23,3 +23,39 @@ type role struct {
 func NewRole(roleRepo repository.Role) Role {
 	return &role{roleRepo}
 }
+
+func (r *role) CreateRole(ctx context.Context, req model.CreateRoleRequest) error {
+	role := model.Role{
+		Name:        req.Name,
+		Description: req.Description,
+		Icon:        req.Icon,
+		Color:       req.Color,
+	}
+	return r.roleRepo.CreateRole(ctx, role)
+}
+
+func (r *role) GetRoleByID(ctx context.Context, id string) (model.Role, error) {
+	return r.roleRepo.GetRoleByID(ctx, id)
+}
+
+func (r *role) GetRoleByName(ctx context.Context, name string) (model.Role, error) {
+	return r.roleRepo.GetRoleByName(ctx, name)
+}
+
+func (r *role) GetRoles(ctx context.Context) ([]model.Role, error) {
+	return r.roleRepo.GetRoles(ctx)
+}
+
+func (r *role) UpdateRole(ctx context.Context, id string, req model.CreateRoleRequest) error {
+	role := model.Role{
+		Name:        req.Name,
+		Description: req.Description,
+		Icon:        req.Icon,
+		Color:       req.Color,
+	}
+	return r.roleRepo.UpdateRole(ctx, id, role)
+}
+
+func (r *role) DeleteRole(ctx context.Context, id string) error {
+	return r.roleRepo.DeleteRole(ctx, id)
+}
